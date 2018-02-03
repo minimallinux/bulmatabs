5d7d8e625b00fa40243f0a6415f0e58e11649921
minimallinux <anthonymcgowan1@gmail.com>
2018-02-03 15:24:32 +0000

    Update

---
+++ README.md
@@ -0,0 +1,2 @@
+# bulmatabs
+Straighforward Tabs using Bulma and FA, with Javascript for selecting and changing tabs and css fade in.
---
+++ js/app.js
@@ -0,0 +1,17 @@
+/*jslint browser:true */
+function openTab(evt, tabTitle) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabTitle).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+
+
